517c15334af345cc8b64bd292927aa350c861787
PaulXuzw <54526512+PaulXuzw@users.noreply.github.com>
2019-09-19 15:41:04 +0800

    Update 70.py

--- leetcode/dp/70.py
+++ leetcode/dp/70.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 2
+        if n == 1:
+            return 1
+        elif n ==2:
+            return 2
+        else:
+            for i in range(n-2):
+                res = a+b
+                a = b
+                b = res
+            return b
