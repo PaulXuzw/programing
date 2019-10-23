7c029f7056115ca6f80d58ed48c9df28e3d7fbd8
PaulXuzw <54526512+PaulXuzw@users.noreply.github.com>
2019-10-23 13:52:30 +0800

    Update 11.py

--- leetcode/doublePoint/11.py
+++ leetcode/doublePoint/11.py
@@ -1,5 +1,9 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        '''
+        双指针法：先计算头尾的面积，当头指针的值小于尾指针时，头指针向后传一个，反之，尾指针向前传。每次移动指针，
+        需要重新计算新面积，大于之前的最大值，替代，否则，继续传指针。
+        '''
         start = 0
         end = len(height)-1
         sum = min(height[start],height[end])
